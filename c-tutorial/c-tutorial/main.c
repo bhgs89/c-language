2149a19eaeb93b66522ad9d94a68dcfcf60adfb7
Baek Han <han.baek.program@gmail.com>
2022-01-03 00:38:31 -0800

    new project

---
+++ c-tutorial/c-tutorial/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c-tutorial
+//
+//  Created by Hangwonsok Baek on 1/3/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
