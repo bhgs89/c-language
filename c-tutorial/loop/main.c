d2a6b487c2ec94ae4c920893544cde515c79df78
Baek Han <han.baek.program@gmail.com>
2022-01-13 21:36:45 -0800

    add while and break

--- c-tutorial/loop/main.c
+++ c-tutorial/loop/main.c
@@ -10,14 +10,15 @@
 //      2-2. 조건이 false이면 { 반복문 } 탈출
 //      3. { 반복문 } 실행 이후, 증감 부분 실행
 //      4. 다시 2-1로 돌아와 수행
+
 int main(void) {
     // 합계 출력 (while)
-        // int i = 1, sum = 0;
-        // while (i <= 1000) {
-        //     sum = sum + i;
-        //     i++;
-        // }
-        // printf("1부터 1000까지의 합은 %d입니다.\n", sum);
+    // int i = 1, sum = 0;
+    // while (i <= 1000) {
+    //     sum = sum + i;
+    //     i++;
+    // }
+    // printf("1부터 1000까지의 합은 %d입니다.\n", sum);
 
     // 사각형 출력 (for)
     //    int i, j;
@@ -29,18 +30,43 @@ int main(void) {
     //    }
     
     // 피라미드 출력 (for)
-    int i, j;
-    for (i = 0; i < 20; i++) {
-        for (j = 20 - i - 1; j > 0; j--) {
-            printf("  ");
+    //    int i, j;
+    //    for (i = 0; i < 20; i++) {
+    //        for (j = 20 - i - 1; j > 0; j--) {
+    //            printf("  ");
+    //        }
+    //        for (j = 0; j < i; j++) {
+    //            printf("* ");
+    //        }
+    //        for (j = 0; j < i - 1; j++) {
+    //            printf("* ");
+    //        }
+    //        printf("\n");
+    //    }
+    
+    // while
+    //    int x = 1;
+    //    while (x < 10) {
+    //        printf("x는 %d입니다\n", x);
+    //        x++;
+    //    }
+    
+    // do while
+    //    int x = 0;
+    //    do {
+    //        printf("x는 %d입니다\n", x);
+    //        x++;
+    //    } while (x < 9);
+    
+    // Break
+    int x = 0;
+    while (x < 10) {
+        if (x == 5) {
+            break;
         }
-        for (j = 0; j < i; j++) {
-            printf("* ");
-        }
-        for (j = 0; j < i - 1; j++) {
-            printf("* ");
-        }
-        printf("\n");
+        
+        printf("x는 %d입니다\n", x);
+        x++;
     }
     
     return 0;
