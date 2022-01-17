e5f8a95445d10a79b13399136788c4ade2e29e62
Baek Han <han.baek.program@gmail.com>
2022-01-16 21:07:03 -0800

    create input and output ex1

--- c-tutorial/input_output/main.c
+++ c-tutorial/input_output/main.c
@@ -1,7 +1,47 @@
+// 기본 입출력 관련 Library
 #include <stdio.h>
 
+// scanf()
+//  Input을 받는 함수 (read input)
+// printf()
+//  Output을 출력한다 (display output)
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // 간단한 계산기
+    char o;
+    int x, y;
+    while (1) { // 무한 반복
+        printf("수식을 입력하세요 : ");
+        // 입력 후, Enter를 누르면 Enter에 관련된 input도 같이 받는다
+        scanf("%d %c %d", &x, &o, &y);
+        
+        if (o == '+') {
+            printf("%d %c %d = %d\n", x, o, y, x + y);
+        } else if (o == '-') {
+            printf("%d %c %d = %d\n", x, o, y, x - y);
+        } else if (o == '*') {
+            printf("%d %c %d = %d\n", x, o, y, x * y);
+        } else if (o == '/') {
+            printf("%d %c %d = %d\n", x, o, y, x / y);
+        } else if (o == '%') {
+            printf("%d %c %d = %d\n", x, o, y, x % y);
+        } else {
+            printf("입력이 잘못 되었습니다.\n");
+        }
+        
+        // Buffer 처리
+        // 단 하나의 문자를 입력받는다
+        getchar();
+        printf("프로그램을 종료하시겠습니까? (y/n) ");
+        scanf("%c", &o);
+        if (o == 'n' || o == 'N') {
+            continue;
+        } else if (o == 'y' || o == 'Y') {
+            break;
+        } else {
+            printf("입력이 잘못 되었습니다.\n");
+        }
+    }
+    
     return 0;
 }
