976838a8c96298c7519568313ed653b79e5bac57
Baek Han <han.baek.program@gmail.com>
2022-01-16 21:12:09 -0800

    add integer multiply

--- c-tutorial/input_output/main.c
+++ c-tutorial/input_output/main.c
@@ -44,16 +44,24 @@ int main(int argc, const char * argv[]) {
     //    }
     
     // 정해진 개수의 정수를 더하는 프로그램
-    int number, x, i, sum = 0;
-    printf("정수의 개수를 입력하세요 : ");
-    scanf("%d", &number);
+    //    int number, x, i, sum = 0;
+    //    printf("정수의 개수를 입력하세요 : ");
+    //    scanf("%d", &number);
+    //
+    //    for (i = 0; i < number; i++) {
+    //        printf("정수의 값을 입력하세요 : ");
+    //        scanf("%d", &x);
+    //        sum += x;
+    //    }
+    //
+    //    printf("전체 정수의 값은 %d입니다.\n", sum);
     
-    for (i = 0; i < number; i++) {
-        printf("정수의 값을 입력하세요 : ");
-        scanf("%d", &x);
-        sum += x;
+    // 특정한 단의 구구단을 출력하는 프로그램
+    int x, i;
+    printf("정수를 입력하세요 : ");
+    scanf("%d", &x);
+    for (i = 1; i <= 9; i++) {
+        printf("%d X %d = %d\n", x, i, x * i);
     }
-    
-    printf("전체 정수의 값은 %d입니다.\n", sum);
     return 0;
 }
