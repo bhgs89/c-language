fbcd67597acb875030489601b0d37d31f01e938f
Baek Han <han.baek.program@gmail.com>
2022-01-16 21:09:48 -0800

    add interger sum program

--- c-tutorial/input_output/main.c
+++ c-tutorial/input_output/main.c
@@ -8,40 +8,52 @@
 
 int main(int argc, const char * argv[]) {
     // 간단한 계산기
-    char o;
-    int x, y;
-    while (1) { // 무한 반복
-        printf("수식을 입력하세요 : ");
-        // 입력 후, Enter를 누르면 Enter에 관련된 input도 같이 받는다
-        scanf("%d %c %d", &x, &o, &y);
-        
-        if (o == '+') {
-            printf("%d %c %d = %d\n", x, o, y, x + y);
-        } else if (o == '-') {
-            printf("%d %c %d = %d\n", x, o, y, x - y);
-        } else if (o == '*') {
-            printf("%d %c %d = %d\n", x, o, y, x * y);
-        } else if (o == '/') {
-            printf("%d %c %d = %d\n", x, o, y, x / y);
-        } else if (o == '%') {
-            printf("%d %c %d = %d\n", x, o, y, x % y);
-        } else {
-            printf("입력이 잘못 되었습니다.\n");
-        }
-        
-        // Buffer 처리
-        // 단 하나의 문자를 입력받는다
-        getchar();
-        printf("프로그램을 종료하시겠습니까? (y/n) ");
-        scanf("%c", &o);
-        if (o == 'n' || o == 'N') {
-            continue;
-        } else if (o == 'y' || o == 'Y') {
-            break;
-        } else {
-            printf("입력이 잘못 되었습니다.\n");
-        }
+    //    char o;
+    //    int x, y;
+    //    while (1) { // 무한 반복
+    //        printf("수식을 입력하세요 : ");
+    //        // 입력 후, Enter를 누르면 Enter에 관련된 input도 같이 받는다
+    //        scanf("%d %c %d", &x, &o, &y);
+    //
+    //        if (o == '+') {
+    //            printf("%d %c %d = %d\n", x, o, y, x + y);
+    //        } else if (o == '-') {
+    //            printf("%d %c %d = %d\n", x, o, y, x - y);
+    //        } else if (o == '*') {
+    //            printf("%d %c %d = %d\n", x, o, y, x * y);
+    //        } else if (o == '/') {
+    //            printf("%d %c %d = %d\n", x, o, y, x / y);
+    //        } else if (o == '%') {
+    //            printf("%d %c %d = %d\n", x, o, y, x % y);
+    //        } else {
+    //            printf("입력이 잘못 되었습니다.\n");
+    //        }
+    //
+    //        // Buffer 처리
+    //        // 단 하나의 문자를 입력받는다
+    //        getchar();
+    //        printf("프로그램을 종료하시겠습니까? (y/n) ");
+    //        scanf("%c", &o);
+    //        if (o == 'n' || o == 'N') {
+    //            continue;
+    //        } else if (o == 'y' || o == 'Y') {
+    //            break;
+    //        } else {
+    //            printf("입력이 잘못 되었습니다.\n");
+    //        }
+    //    }
+    
+    // 정해진 개수의 정수를 더하는 프로그램
+    int number, x, i, sum = 0;
+    printf("정수의 개수를 입력하세요 : ");
+    scanf("%d", &number);
+    
+    for (i = 0; i < number; i++) {
+        printf("정수의 값을 입력하세요 : ");
+        scanf("%d", &x);
+        sum += x;
     }
     
+    printf("전체 정수의 값은 %d입니다.\n", sum);
     return 0;
 }
