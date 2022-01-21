1c138aa2c4c746260f5120a06a8368279e25c77c
Baek Han <han.baek.program@gmail.com>
2022-01-20 19:12:45 -0800

    Add gugudan

--- c-tutorial/multi_array/main.c
+++ c-tutorial/multi_array/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+// 다차원 배열 (Multi Array)
+//  - N X M Elements
+
+// 1차원 배열
+// [][][][][][][][][][]
+
+// 2차원 배열
+// [][][][][][][][][][]
+// [][][][][][][][][][]
+// [][][][][][][][][][]
+// [][][][][][][][][][]
+// [][][][][][][][][][]
 
 int main(int argc, const char * argv[]) {
-    printf("Hello, World!\n");
+    // 구구단을 이용하여 2차원 배열 출력하기
+    //  i = row, j = column
+    int i, j;
+    int gugudan[10][10];
+    for (i = 1; i <= 9; i++) {
+        printf("\n[ %d단 ]\n\n", i);
+        for (j = 1; j <= 9; j++) {
+            gugudan[i][j] = i * j;
+            printf("%d X %d = %d\n", i, j, gugudan[i][j]);
+        }
+    }
+    
     return 0;
 }
